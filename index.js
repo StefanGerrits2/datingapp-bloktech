cc1bb3e22d00fd60905b24b274a946a99296bf12
Stefan Gerrits <stefangerrits2000@gmail.com>
2019-03-12 12:07:14 +0100

    Added a form

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = 3000;
 app.set('view engine', 'pug'); //middleware bovenaan zetten zodat hij weet welke template engine er wordt gebruikt
 app.use('/static', express.static('static'));
 app.get('/', index)
-app.get('/profile', profile)
+app.get('/profiel', profiel)
 app.get('/matches', matches)
 app.get('/inbox', inbox)
 app.get('/zoeken', zoeken)
@@ -22,8 +22,8 @@ app.get('/zoeken', zoeken)
 function index (req, res) {
 	res.render('index.pug');
 }
-function profile (req, res) {
-	res.render('profile.pug');
+function profiel (req, res) {
+	res.render('profiel.pug');
 }
 function matches (req, res) {
 	res.render('matches.pug');
