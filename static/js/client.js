191b2b0b198183525a99d5b7f15ed290b2bc79dd
Stefan Gerrits <stefangerrits2000@gmail.com>
2019-04-08 14:14:41 +0200

    Add javascript enhancement

--- static/js/client.js
+++ static/js/client.js
@@ -26,12 +26,31 @@ function onremove(e) {
   } 
 }
 
-//if javascript is enabled:
-var enhance = getElementById('enhance');
-	
-enhance.classList.toggle('js-enabled');
-     	
 
+var body = document.querySelector('body');
+body.classList.remove('no-js');
 
+var button1 = document.querySelector('.button1');
+var button2 = document.querySelector('.button2');
 
+var form1 = document.querySelector('.form1');
+var form2 = document.querySelector('.form2');
+var form3 = document.querySelector('.form3');
 
+// Show form 2
+function showform2(){
+  event.preventDefault();
+  form1.classList.toggle('hide');
+  form2.classList.toggle('show');
+}
+
+button1.addEventListener('click', showform2, false);
+
+// Show form 3
+function showform3(){
+  event.preventDefault();
+  form2.classList.toggle('hide');
+  form3.classList.toggle('show');
+}
+
+button2.addEventListener('click', showform3, false);
